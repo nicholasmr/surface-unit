rounded state fields, changed speed avg. calculation, and some bug fixes

drill-control.py
@@ -16,13 +16,13 @@ import pyqtgraph as pg
 # Settings
 #-------------------
 
-DT           = 1/20 # update rate in seconds for GUI/surface state
-DTFRAC_DRILL = 10 # update the drill state every DTFRAC_DRILL times the GUI/surface state is updated
+DT           = 1/10 # update rate in seconds for GUI/surface state
+DTFRAC_DRILL = 5 # update the drill state every DTFRAC_DRILL times the GUI/surface state is updated
 XAXISLEN     = 30 + 1 # seconds
 
 SHOW_BNO055_DETAILED = 1
 
-FS = 12
+FS = 14
 FS_GRAPH_TITLE = 4 # font size for graph titles
 PATH_SCREENSHOT = "/mnt/logs/screenshots"
 
@@ -51,10 +51,9 @@ class MainWidget(QWidget):
 
         ### State objects
 
+        # REDIS_HOST determined in settings file
         self.ds = DrillState(  redis_host=REDIS_HOST)   
         self.ss = SurfaceState(redis_host=REDIS_HOST)
-        
-#        print(dir(self.ds))
 
         ### pyqt graphs
 
@@ -64,10 +63,10 @@ class MainWidget(QWidget):
         # X-axis
         self.hist_time       = np.flipud(np.arange(0,XAXISLEN+1e-9,DT))
         self.hist_time_drill = np.flipud(np.arange(0,XAXISLEN+1e-9,DT*DTFRAC_DRILL))
-        self.hist_load     = np.full(len(self.hist_time),np.nan)
-        self.hist_speed    = np.full(len(self.hist_time),np.nan)
-        self.hist_speedavg = np.full(len(self.hist_time),np.nan)
-        self.hist_current  = np.full(len(self.hist_time_drill),np.nan)
+        self.hist_load     = np.full(len(self.hist_time), 0.0)
+        self.hist_speed    = np.full(len(self.hist_time), 0.0)
+        self.hist_speedavg = np.full(len(self.hist_time), 0.0)
+        self.hist_current  = np.full(len(self.hist_time_drill), 0.0)
 
         def setupaxis(obj):
             obj.invertX()
@@ -84,12 +83,11 @@ class MainWidget(QWidget):
         setupaxis(self.plot_current);
 
         # init curves
-        lw = 4
+        lw = 3
         plotpen_black = pg.mkPen(color='k', width=lw)
         plotpen_blue  = pg.mkPen(color=COLOR_BLUE, width=lw-1)
         self.curve_load     = self.plot_load.plot(    x=self.hist_time,y=self.hist_time*0-1e4, pen=plotpen_black)
         self.curve_speed    = self.plot_speed.plot(   x=self.hist_time,y=self.hist_time*0-1e4, pen=plotpen_black)
-        self.curve_speedavg = self.plot_speed.plot(   x=self.hist_time,y=self.hist_time*0-1e4, pen=plotpen_blue)
         self.curve_current  = self.plot_current.plot( x=self.hist_time_drill,y=self.hist_time_drill*0-1e4, pen=plotpen_black)
         
         # Titles
@@ -156,8 +154,7 @@ class MainWidget(QWidget):
         layout.addWidget(self.MakeStateBox('surface_depth',           'Depth (m)',            initstr))
         layout.addWidget(self.MakeStateBox('surface_load',            'Load (kg)',            initstr))
         layout.addWidget(self.MakeStateBox('surface_loadcable',       'Load - cable (kg)',    initstr))
-        layout.addWidget(self.MakeStateBox('surface_speedinst',       'Speed, instant. (cm/s)',  initstr))
-        layout.addWidget(self.MakeStateBox('surface_speedavg',        'Speed, average (cm/s)',   initstr))
+        layout.addWidget(self.MakeStateBox('surface_speed',           'Speed (cm/s)',         initstr))
         layout.addWidget(self.MakeStateBox('surface_downholevoltage', 'Downhole voltage (V)', initstr))
         layout.addStretch(1)
         self.gb_surface.setLayout(layout)
@@ -441,30 +438,27 @@ class MainWidget(QWidget):
         self.ss.update()
 
         ### Update graphs
-        self.hist_load     = np.roll(self.hist_load,     -1); self.hist_load[-1]     = self.ss.load-self.ss.loadtare if self.cbox_plotdeltaload.isChecked() else self.ss.load
-        self.hist_speed    = np.roll(self.hist_speed,    -1); self.hist_speed[-1]    = self.ss.velocity  
-        self.hist_speedavg = np.roll(self.hist_speedavg, -1); self.hist_speedavg[-1] = self.ss.velocityavg
-        self.curve_load.setData(     x=self.hist_time,y=self.hist_load)
-        self.curve_speed.setData(    x=self.hist_time,y=self.hist_speed)
-        self.curve_speedavg.setData( x=self.hist_time,y=self.hist_speedavg)
+        self.hist_load  = np.roll(self.hist_load,  -1); self.hist_load[-1]  = self.ss.load-self.ss.loadtare if self.cbox_plotdeltaload.isChecked() else self.ss.load
+        self.hist_speed = np.roll(self.hist_speed, -1); self.hist_speed[-1] = self.ss.speed*100
+        self.curve_load.setData( x=self.hist_time,y=self.hist_load)
+        self.curve_speed.setData(x=self.hist_time,y=self.hist_speed)
 
         ### Update state fields
-        self.updateStateBox('surface_depth',           self.ss.depth,            warn__nothres)
-        self.updateStateBox('surface_speedinst',       self.ss.velocity,         warn__velocity)
-        self.updateStateBox('surface_speedavg',        self.ss.velocityavg,      warn__velocity)
-        self.updateStateBox('surface_load',            self.ss.load,             warn__load)
-        self.updateStateBox('surface_loadcable',       self.ss.loadnet,          warn__nothres)
-        self.updateStateBox('surface_downholevoltage', round(self.ds.downhole_voltage,1), warn__nothres)
+        self.updateStateBox('surface_depth',           round(self.ss.depth,PRECISION_DEPTH),  warn__nothres)  # precision to match physical display
+        self.updateStateBox('surface_speed',           round(self.ss.speed*100,2),            warn__velocity)
+        self.updateStateBox('surface_load',            round(self.ss.load,PRECISION_LOAD),    warn__load) # precision to match physical display
+        self.updateStateBox('surface_loadcable',       round(self.ss.loadnet,PRECISION_LOAD), warn__nothres)
+        self.updateStateBox('surface_downholevoltage', round(self.ds.downhole_voltage,1),     warn__nothres)
 
         if self.btn_startrun.isChecked(): self.runtime1 = datetime.now() # update run time
         if self.runtime0 is not None:
             druntime = self.runtime1-self.runtime0
-            self.updateStateBox('run_time',       self.timestamp(druntime),          warn__nothres)
-            self.updateStateBox('run_startdepth', self.ss.depthtare,                 warn__nothres)    
-            self.updateStateBox('run_startload',  self.ss.loadtare,                  warn__nothres)    
-            self.updateStateBox('run_deltadepth', self.ss.depth - self.ss.depthtare, warn__corelength)    
-            self.updateStateBox('run_deltaload',  self.ss.load  - self.ss.loadtare,  warn__nothres)
-            self.updateStateBox('run_peakload',   np.amax(self.ss.load),             warn__nothres)
+            self.updateStateBox('run_time',       self.timestamp(druntime),                                 warn__nothres)
+            self.updateStateBox('run_startdepth', round(self.ss.depthtare,PRECISION_DEPTH),                 warn__nothres)    
+            self.updateStateBox('run_startload',  round(self.ss.loadtare,PRECISION_LOAD),                   warn__nothres)    
+            self.updateStateBox('run_deltadepth', round(self.ss.depth - self.ss.depthtare,PRECISION_DEPTH), warn__corelength)    
+            self.updateStateBox('run_deltaload',  round(self.ss.load  - self.ss.loadtare,PRECISION_LOAD),   warn__nothres)
+            self.updateStateBox('run_peakload',   round(np.amax(self.hist_load),PRECISION_LOAD),            warn__nothres)
             
 
         #-----------------------
@@ -498,19 +492,19 @@ class MainWidget(QWidget):
             else:
                 self.updateStateBox('orientation_inclinometer', str_incvec,          warn__nothres)
 
-            self.updateStateBox('pressure_electronics', self.ds.pressure_electronics, warn__pressure)
-            self.updateStateBox('pressure_topplug',     self.ds.pressure_topplug,     warn__pressure)
-            self.updateStateBox('pressure_gear1',       self.ds.pressure_gear1,       warn__pressure)
-            self.updateStateBox('pressure_gear2',       self.ds.pressure_gear2,       warn__pressure)
-            self.updateStateBox('hammer',               round(self.ds.hammer,1),      warn__hammer)
+            self.updateStateBox('pressure_electronics', round(self.ds.pressure_electronics,1), warn__pressure)
+            self.updateStateBox('pressure_topplug',     round(self.ds.pressure_topplug,1),     warn__pressure)
+            self.updateStateBox('pressure_gear1',       round(self.ds.pressure_gear1,1),       warn__pressure)
+            self.updateStateBox('pressure_gear2',       round(self.ds.pressure_gear2,1),       warn__pressure)
+            self.updateStateBox('hammer',               round(self.ds.hammer,1),               warn__hammer)
 
-            self.updateStateBox('temperature_electronics',    self.ds.temperature_electronics,    warn__temperature_electronics)
-            self.updateStateBox('temperature_topplug',        self.ds.temperature_topplug,        warn__temperature_electronics)
-            self.updateStateBox('temperature_gear1',          self.ds.temperature_gear1,          warn__temperature_electronics)
-            self.updateStateBox('temperature_gear2',          self.ds.temperature_gear1,          warn__temperature_electronics)
-            self.updateStateBox('temperature_auxelectronics', self.ds.temperature_auxelectronics, warn__temperature_electronics)
-            self.updateStateBox('temperature_motor',          self.ds.temperature_motor,          warn__temperature_motor)    
-            self.updateStateBox('temperature_motorctrl',      self.ds.motor_controller_temp,      warn__temperature_motor)    
+            self.updateStateBox('temperature_electronics',    round(self.ds.temperature_electronics,1),    warn__temperature_electronics)
+            self.updateStateBox('temperature_topplug',        round(self.ds.temperature_topplug,1),        warn__temperature_electronics)
+            self.updateStateBox('temperature_gear1',          round(self.ds.temperature_gear1,1),          warn__temperature_electronics)
+            self.updateStateBox('temperature_gear2',          round(self.ds.temperature_gear1,1),          warn__temperature_electronics)
+            self.updateStateBox('temperature_auxelectronics', round(self.ds.temperature_auxelectronics,1), warn__temperature_electronics)
+            self.updateStateBox('temperature_motor',          round(self.ds.temperature_motor,1),          warn__temperature_motor)    
+            self.updateStateBox('temperature_motorctrl',      round(self.ds.motor_controller_temp,1),      warn__temperature_motor)    
             
             self.updateStateBox('motor_current',  round(self.ds.motor_current,1),  warn__motor_current)
             self.updateStateBox('motor_speed',    round(self.ds.motor_rpm,1),      warn__motor_rpm)    
@@ -542,6 +536,9 @@ if __name__ == '__main__':
     
     main = MainWidget()
     main.show()
+    H = QDesktopWidget().availableGeometry().height()
+    W = 0 # setting width = 0 effectively sets the minimal window width allowed by the widgets enclosed
+    main.setGeometry(0, 0, W, H)
     
     # Update main window with latest field values ever DT seconds
     timer1 = QTimer()

state_drill.py
@@ -5,26 +5,29 @@ import numpy as np
 from settings import *
 
 try: 
+    # might fail if module not available or clock is incorrect set and reference magnetic field geoid (for a given date) is therefore unavailable.
     from ahrs.filters import SAAM
     from ahrs import Quaternion
-except ImportError: 
+except: 
     USE_BNO055_FOR_ORIENTATION = False
 
 class DrillState():
 
     # State variables
-    motor_rpm     = 0
-    motor_voltage = 0
-    motor_current = 0
+    motor_rpm             = 0
+    motor_voltage         = 0
+    motor_current         = 0
     motor_controller_temp = 0
+    motor_duty_cycle      = 0
 
-    temperature_electronics = 0     
-    temperature_topplug     = 0
-    temperature_gear1       = 0
-    temperature_gear2       = 0
-    temperature_baseplate   = 0
-    temperature_motor       = 0
-    temperature_vesc        = 0
+    temperature_electronics    = 0     
+    temperature_auxelectronics = 0
+    temperature_topplug        = 0
+    temperature_gear1          = 0
+    temperature_gear2          = 0
+    temperature_baseplate      = 0
+    temperature_motor          = 0
+    temperature_vesc           = 0
     
     pressure_electronics = 0
     pressure_topplug     = 0
@@ -33,50 +36,75 @@ class DrillState():
     
     hammer = 0
     
-    inclination = 0
-    azimuth     = 0
-    spin        = 0
-    quat        = [0,0,0,0]
-    drilldir    = [0,0,1]
+    inclination = 0 # calculated value, not direct sensor value
+    azimuth     = 0 # calculated value, not direct sensor value
+    spin        = 0 # = gyroscope_z
+    quat        = [0,0,0,0] # calculated using AHRS
+    drilldir    = [0,0,1]   # calculated using AHRS
+    
+    downhole_voltage = 0.0
+    
+    # BNO055 triaxial values
+    accelerometer_x = 0
+    accelerometer_y = 0
+    accelerometer_z = 0
+    magnetometer_x = 0
+    magnetometer_y = 0
+    magnetometer_z = 0
+    gyroscope_x = 0
+    gyroscope_y = 0
+    gyroscope_z = 0
+    
+    # Inclinometer
+    inclination_x = 0
+    inclination_y = 0
     
     # Redis connection
     rc = None 
     
     ###
     
-    def __init__(self, redis_host='127.0.0.1'):
-        self.rc = redis.StrictRedis(host=redis_host) # redis connection (rc) object
-        self.saam = SAAM() # https://ahrs.readthedocs.io/en/latest/filters/saam.html
-        self.refdir = np.array([0,0,1]) # used to determine orientation with SAAM
+    def __init__(self, redis_host=LOCAL_HOST):
+    
+        # redis connection (rc) object
+        try:    
+            self.rc = redis.StrictRedis(host=redis_host) 
+            self.rc.ping() 
+        except:
+            print('DrillState(): redis connection to %s failed. Using %s instead.'%(redis_host,LOCAL_HOST))
+            self.rc = redis.StrictRedis(host=LOCAL_HOST) 
+
+        if USE_BNO055_FOR_ORIENTATION: self.saam = SAAM() # https://ahrs.readthedocs.io/en/latest/filters/saam.html
+        self.refdir = np.array([0,0,1]) # BNO055 chip orientation; used to determine orientation with SAAM
         self.update()
 
     def get(self, attr):
         try:    return getattr(self, attr)
         except: return None
             
-    def update(self, skipOrientation=False):
+    def update(self):
     
         try:    ds = json.loads(self.rc.get('drill-state')) # redis state
         except: ds = {}
         for key in ds: setattr(self, key, ds[key])
 #        print(ds)
-            
+
         # orientation
         self.spin = round(abs(self.get_spin()), 2)
-        if not skipOrientation: 
-            self.inclination, self.azimuth = self.get_orientation() # might be heavy calculation, allowing skipping it if requested.
-            self.accelerometer_magnitude = np.sqrt(self.accelerometer_x**2 + self.accelerometer_y**2 + self.accelerometer_z**2)
-            self.magnetometer_magnitude  = np.sqrt(self.magnetometer_x**2  + self.magnetometer_y**2  + self.magnetometer_z**2)
-            self.gyroscope_magnitude     = np.sqrt(self.gyroscope_x**2     + self.gyroscope_y**2     + self.gyroscope_z**2)
+        self.inclination, self.azimuth = self.get_orientation() # might be heavy calculation, allowing skipping it if requested.
+        self.accelerometer_magnitude = np.sqrt(self.accelerometer_x**2 + self.accelerometer_y**2 + self.accelerometer_z**2)
+        self.magnetometer_magnitude  = np.sqrt(self.magnetometer_x**2  + self.magnetometer_y**2  + self.magnetometer_z**2)
+        self.gyroscope_magnitude     = np.sqrt(self.gyroscope_x**2     + self.gyroscope_y**2     + self.gyroscope_z**2)
 
         # motor 
         self.motor_throttle = 100 * self.motor_duty_cycle
 
         # rename
-        self.temperature_auxelectronics = self.aux_temperature_electronics        
-        self.temperature_topplug        = self.aux_temperature_topplug
-        self.temperature_gear1          = self.aux_temperature_gear1
-        self.temperature_gear2          = self.aux_temperature_gear2
+        if hasattr(self, 'aux_temperature_electronics'):
+            self.temperature_auxelectronics = self.aux_temperature_electronics        
+            self.temperature_topplug        = self.aux_temperature_topplug
+            self.temperature_gear1          = self.aux_temperature_gear1
+            self.temperature_gear2          = self.aux_temperature_gear2
         
         # aux
         self.hammer      = 100 * self.hammer/HAMMER_MAX
@@ -117,9 +145,8 @@ class DrillState():
     ### Orientation
     
     def get_spin(self):
-        # z-component of angular velocity vector, i.e. spin about z-axis (deg/s)
-        try:    return self.gyroscope_z * 1/6 # convert deg/s to RPM
-        except: return 0 # BNO055 data not available? Shall not attempt to determine spin from inclinometer...
+        # z-component of angular velocity vector, i.e. spin about drill (z) axis (deg/s)
+        return self.gyroscope_z * 1/6 # convert deg/s to RPM (will be zero if USE_BNO055_FOR_ORIENTATION is false)
 
     def get_orientation(self, USE_AHRS=True):
     

state_surface.py
@@ -1,4 +1,4 @@
-# Nicholas Rathmann, 2022
+# N. Rathmann <rathmann@nbi.dk>, 2019-2022
 
 import redis, json, time
 import numpy as np
@@ -6,28 +6,35 @@ from settings import *
 
 class SurfaceState():
 
-    depth     = 0
-    depthtare = 0
+    depth     = 0.0
+    depthtare = 0.0
     
-    load     = 0
-    loadtare = 0
-    loadnet  = 0
-    
-    velocity    = 0 # instantaneous
-    velocityavg = 0 # time-average
-    
-    # For avg velocity calculation
+    load     = 0.0
+    loadtare = 0.0
+    loadnet  = 0.0
+
+    # For speed calculation    
+    speed = 0.0 # instantaneous
     Navg = 10
-    arr_velocity = np.zeros((Navg))  
-    arr_time     = np.zeros((Navg))   
+    arr_speed = np.zeros((Navg))  
+    arr_time  = np.zeros((Navg))   
+    depthprev = 0.0
 
     # Redis connection
     rc = None 
     
     ###
     
-    def __init__(self, redis_host='127.0.0.1'):
-        self.rc = redis.StrictRedis(host=redis_host) # redis connection (rc) object
+    def __init__(self, redis_host=LOCAL_HOST):
+    
+        # redis connection (rc) object
+        try:    
+            self.rc = redis.StrictRedis(host=redis_host) 
+            self.rc.ping() 
+        except:
+            print('SurfaceState(): redis connection to %s failed. Using %s instead.'%(redis_host,LOCAL_HOST))
+            self.rc = redis.StrictRedis(host=LOCAL_HOST) 
+
         self.update()
 
     def get(self, attr):
@@ -36,25 +43,26 @@ class SurfaceState():
             
     def update(self):
     
+        self.depthprev = self.depth # for speed calculation
+    
         try: 
             encoder = json.loads(self.rc.get('depth-encoder'))
-            self.depth     = abs(encoder["depth"])
-            self.velocity  = abs(encoder["velocity"])
+            self.depth = abs(encoder["depth"])
             try:    self.depthtare = float(self.rc.get('depth-tare'))
             except: self.depthtare = self.depth
         except:
-            self.depth, self.velocity, self.depthtare = 0,0,0
+            self.depth, self.depthtare = 0.0, 0.0
 
-        self.calc_velocityavg(self.velocity)
+        self.speed = self.calc_speed(self.depth, self.depthprev)
 
         try:
             loadcell = json.loads(self.rc.get('load-cell'))
-            self.load = loadcell["load"]
+            self.load = float(loadcell["load"])
             self.loadnet  = self.load - CABLE_DENSITY*self.depth
             try:    self.loadtare = float(self.rc.get('load-tare'))
             except: self.loadtare = self.load
         except:
-            self.load, self.loadnet, self.loadtare = 0,0,0
+            self.load, self.loadnet, self.loadtare = 0.0, 0.0, 0.0
         
         try:    self.alertloggers = int(self.rc.get('alert-loggers'))
         except: self.alertloggers = 0
@@ -71,16 +79,19 @@ class SurfaceState():
     def toggle_alertloggers(self):
         self.set_alertloggers(not self.alertloggers)
         
-    def calc_velocityavg(self, velocity_inst):
-    
-        self.arr_velocity = np.roll(self.arr_velocity, -1); 
-        self.arr_velocity[-1] = velocity_inst
-    
+    def calc_speed(self, depth, depthprev):
+
         self.arr_time = np.roll(self.arr_time, -1); 
         self.arr_time[-1] =  time.time() # seconds stamp
-        
+    
+        speed_inst = (depth-depthprev)/(self.arr_time[-1]-self.arr_time[-2])
+    
+        self.arr_speed = np.roll(self.arr_speed, -1); 
+        self.arr_speed[-1] = speed_inst
+       
         dt = np.diff(self.arr_time)
         T = np.sum(dt)
-        self.velocityavg = np.sum(np.multiply(self.arr_velocity[1:],dt))/T
-
+        speed_avg = np.sum(np.multiply(self.arr_speed[1:],dt))/T
+        
+        return speed_avg
     